8cdf3260941f3baec0b7a5388e9fcc8ccf7fa7ec
Yi Lu <luyi0619@outlook.com>
2014-02-26 13:16:12 +0800

    blogel pagerank

---
+++ apps/pagerank/blogel/run.cpp
@@ -0,0 +1,10 @@
+//#include "blogel_app_pagerank1.h"
+#include "blogel_app_pagerank2.h"
+int main(int argc, char* argv[])
+{
+    init_workers();
+    //blogel_app_pagerank1("","");
+    blogel_app_pagerank2("","");
+    worker_finalize();
+    return 0;
+}
